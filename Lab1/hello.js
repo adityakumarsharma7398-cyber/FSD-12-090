d3422c17e811a34889321193e4fa6065c252dc71
adityakumatsharma7398-cyber <adityakumarsharma7398@gmail.com>
2026-07-23 11:54:13 +0530

    initial commit

---
+++ Lab1/hello.js
@@ -0,0 +1,18 @@
+console.log("Aditya kumar Sharma");
+document.write("Aditya Kumar Sharma");
+
+function sum(a,b){
+    return a+ b;
+
+}
+
+const add =(a,b)=>{
+    return a+b;
+
+};
+
+const add2 = (a,b) => a+b;
+
+console.log(sum(20,10));
+console.log(sum(20, 20));
+console.log(sum(20, 30));
