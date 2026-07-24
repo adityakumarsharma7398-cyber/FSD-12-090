de7ccc94440aa615874baeac8ca72fa9f7cabe2e
adityakumatsharma7398-cyber <adityakumarsharma7398@gmail.com>
2026-07-24 15:09:26 +0530

    "new commit"

--- Lab1/hello.js
+++ Lab1/hello.js
@@ -17,22 +17,3 @@ console.log(sum(20,10));
 console.log(sum(20, 20));
 console.log(sum(20, 30));
 
-
-function numberToWord(num) {
-    switch (num) {
-        case 0: return "Zero";
-        case 1: return "One";
-        case 2: return "Two";
-        case 3: return "Three";
-        case 4: return "Four";
-        case 5: return "Five";
-        case 6: return "Six";
-        case 7: return "Seven";
-        case 8: return "Eight";
-        case 9: return "Nine";
-        default: return "Invalid Number";
-    }
-}
-
-let n = 5;
-console.log(numberToWord(n));
